a205b8ca21c7edb096edfcf63dddaeab2cfb9981
hoang.ngo <hoanggg2110@gmail.com>
2025-03-28 13:34:20 +0700

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,158 @@
+import { useState } from "react"
+import { clsx } from "clsx"
+import { languages } from "./languages"
+import { getFarewellText, getRandomWord } from "./utils"
+import Confetti from "react-confetti"
+/**
+ * Goal: Add in the incorrect guesses mechanism to the game
+ * 
+ * Challenge: When mapping over the languages, determine how
+ * many of them have been "lost" and add the "lost" class if
+ * so.
+ * 
+ * Hint: use the wrongGuessCount combined with the index of
+ * the item in the array while inside the languages.map code
+ */
+
+export default function AssemblyEndgame() {
+    // State values
+    const [currentWord, setCurrentWord] = useState(getRandomWord())
+    const [guessedLetters, setGuessedLetters] = useState([])
+
+
+    // Derived values
+    const wrongGuessCount =
+        guessedLetters.filter(letter => !currentWord.includes(letter)).length
+    const isGameWon = currentWord.split("").every(letter => guessedLetters.includes(letter))
+    const isGameLost = wrongGuessCount >= languages.length - 1
+    const isGameOver = isGameWon || isGameLost
+    const isLastGuessIncorrect = guessedLetters.length > 0 && !currentWord.includes(guessedLetters[guessedLetters.length - 1])
+
+    // Static values
+    const alphabet = "abcdefghijklmnopqrstuvwxyz"
+
+    function resetGame() {
+        setCurrentWord(getRandomWord())
+        setGuessedLetters([])
+        setIsGameOver(false)
+        setIsGameWon(false)
+    }
+
+
+    function addGuessedLetter(letter) {
+        setGuessedLetters(prevLetters =>
+            prevLetters.includes(letter) ?
+                prevLetters :
+                [...prevLetters, letter]
+        )
+    }
+
+    const languageElements = languages.map((lang, index) => {
+        const isLanguageLost = index < wrongGuessCount
+        const styles = {
+            backgroundColor: lang.backgroundColor,
+            color: lang.color
+        }
+        return (
+            <span
+                className={`chip ${isLanguageLost ? "lost" : ""}`}
+                style={styles}
+                key={lang.name}
+            >
+                {lang.name}
+            </span>
+        )
+    })
+
+    const letterElements = currentWord.split("").map((letter, index) => {
+        const shouldRevealLetter = isGameLost || guessedLetters.includes(letter)
+
+        return (<span key={index}>
+            {shouldRevealLetter ? letter.toUpperCase() : ""}
+        </span>)
+    })
+
+    const keyboardElements = alphabet.split("").map(letter => {
+        const isGuessed = guessedLetters.includes(letter)
+        const isCorrect = isGuessed && currentWord.includes(letter)
+        const isWrong = isGuessed && !currentWord.includes(letter)
+        const className = clsx({
+            correct: isCorrect,
+            wrong: isWrong
+        })
+
+        return (
+            <button
+                className={className}
+                key={letter}
+                disabled={isGameOver}
+                aria-disabled={guessedLetters.includes(letter)}
+                aria-label={`Letter ${letter}`}
+                onClick={() => addGuessedLetter(letter)}
+            >
+                {letter.toUpperCase()}
+            </button>
+        )
+    })
+
+    const gameStatusClass = clsx("game-status", {
+        won: isGameWon,
+        lost: isGameLost,
+        farewell: !isGameOver && isLastGuessIncorrect
+    })
+
+    function renderGameStatus() {
+        if (!isGameOver && isLastGuessIncorrect) {
+            return <p className="farewell-message">{getFarewellText(wrongGuessCount - 1)}</p>
+        }
+
+        if (isGameWon) {
+            return (
+                <>
+                    <h2>You win!</h2>
+                    <p>Well done! 🎉</p>
+                </>
+            )
+        }
+        if (isGameLost) {
+            return (
+                <>
+                    <h2>Game over!</h2>
+                    <p>You lose! Better start learning Assembly 😭</p>
+                </>
+            )
+        }
+        return null
+    }
+
+    return (
+        <main>
+            {
+                isGameWon && <Confetti
+                    recycle={false}
+                    numberOfPieces={1000}
+                    width={window.innerWidth}
+                    height={window.innerHeight}
+                />
+            }
+            <header>
+                <h1>Assembly: Endgame</h1>
+                <p>Guess the word within 8 attempts to keep the
+                    programming world safe from Assembly!</p>
+            </header>
+            <section className={gameStatusClass}>
+                {renderGameStatus()}
+            </section >
+            <section className="language-chips">
+                {languageElements}
+            </section>
+            <section className="word">
+                {letterElements}
+            </section>
+            <section className="keyboard">
+                {keyboardElements}
+            </section>
+            {isGameOver && <button className="new-game" onClick={resetGame}>New Game</button>}
+        </main >
+    )
+}
